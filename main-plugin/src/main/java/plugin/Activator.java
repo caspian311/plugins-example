c5b825943ccb5a83e139426ab20df962d0bcd93e
matt.todd <matt.c.todd@gmail.com>
2009-08-15 08:31:20 -0500

    trying to clean things up
    	new file:   .gitignore
    	modified:   main-plugin/.classpath
    	modified:   main-plugin/pom.xml
    	modified:   main-plugin/src/main/java/plugin/Activator.java
    	deleted:    main-plugin/target/classes/plugin/Activator$1.class
    	deleted:    main-plugin/target/classes/plugin/Activator$2.class
    	deleted:    main-plugin/target/classes/plugin/Activator.class
    	deleted:    main-plugin/target/classes/plugin/Widget.class
    	deleted:    main-plugin/target/classes/plugin/WidgetContstants.class
    	deleted:    main-plugin/target/classes/plugin/WidgetDisplayer$1.class
    	deleted:    main-plugin/target/classes/plugin/WidgetDisplayer.class
    	deleted:    main-plugin/target/main-plugin-0.0.1-SNAPSHOT.jar
    	deleted:    main-plugin/target/maven-archiver/pom.properties
    	modified:   sub-plugin1/.classpath
    	modified:   sub-plugin1/.settings/org.eclipse.jdt.core.prefs
    	deleted:    sub-plugin1/target/classes/subplugin/Activator.class
    	deleted:    sub-plugin1/target/classes/subplugin/SubWidget.class
    	deleted:    sub-plugin1/target/maven-archiver/pom.properties
    	deleted:    sub-plugin1/target/sub-plugin1-0.0.1-SNAPSHOT.jar
    	modified:   sub-plugin2/.classpath
    	modified:   sub-plugin2/.settings/org.eclipse.jdt.core.prefs
    	deleted:    sub-plugin2/target/classes/subplugin2/Activator.class
    	deleted:    sub-plugin2/target/classes/subplugin2/SubWidget2.class
    	deleted:    sub-plugin2/target/maven-archiver/pom.properties
    	deleted:    sub-plugin2/target/sub-plugin2-0.0.1-SNAPSHOT.jar

--- main-plugin/src/main/java/plugin/Activator.java
+++ main-plugin/src/main/java/plugin/Activator.java
@@ -1,11 +1,13 @@
 package plugin;
 
+import java.util.List;
+
+import net.todd.osgi.platform.IServiceEventHandler;
+import net.todd.osgi.platform.util.ServiceRegistryUtil;
+
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.Constants;
-import org.osgi.framework.Filter;
 import org.osgi.framework.ServiceEvent;
-import org.osgi.framework.ServiceListener;
 import org.osgi.framework.ServiceReference;
 
 public class Activator implements BundleActivator {
@@ -13,24 +15,6 @@ public class Activator implements BundleActivator {
 
 	public void start(final BundleContext context) throws Exception {
 		displayer = new WidgetDisplayer();
-		displayer.show();
-
-		Filter widgetFilter = context.createFilter("(" + Constants.OBJECTCLASS + "="
-				+ Widget.class.getName() + ")");
-
-		ServiceReference[] allServiceReferences = context.getAllServiceReferences(null,
-				widgetFilter.toString());
-		if (allServiceReferences != null) {
-			for (ServiceReference serviceReference : allServiceReferences) {
-				handleServiceEvent(context, serviceReference, ServiceEvent.REGISTERED);
-			}
-		}
-		context.addServiceListener(new ServiceListener() {
-			public void serviceChanged(ServiceEvent serviceEvent) {
-				handleServiceEvent(context, serviceEvent.getServiceReference(), serviceEvent
-						.getType());
-			}
-		}, widgetFilter.toString());
 		displayer.addCloseListener(new Runnable() {
 			public void run() {
 				try {
@@ -40,22 +24,28 @@ public class Activator implements BundleActivator {
 				}
 			}
 		});
+		displayer.show();
+
+		List<Widget> implementations = ServiceRegistryUtil.getServiceImplementations(context,
+				Widget.class);
+		for (Widget widget : implementations) {
+			displayer.addWidget(widget);
+		}
+
+		ServiceRegistryUtil.addServiceListener(context, Widget.class, new IServiceEventHandler() {
+			public void handleServiceEvent(BundleContext context,
+					ServiceReference serviceReference, int eventType) {
+				Widget widget = (Widget) context.getService(serviceReference);
+				if (eventType == ServiceEvent.REGISTERED) {
+					displayer.addWidget(widget);
+				} else if (eventType == ServiceEvent.UNREGISTERING) {
+					displayer.removeWidget(widget);
+				}
+			}
+		});
 	}
 
 	public void stop(BundleContext context) throws Exception {
 		displayer.hide();
-	}
-
-	private void handleServiceEvent(final BundleContext context, ServiceReference serviceReference,
-			int eventType) {
-		Widget widget = (Widget) context.getService(serviceReference);
-		switch (eventType) {
-			case ServiceEvent.REGISTERED:
-				displayer.addWidget(widget);
-				break;
-			case ServiceEvent.UNREGISTERING:
-				displayer.removeWidget(widget);
-				break;
-		}
 	}
 }
